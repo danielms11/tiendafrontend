48297b39b9504c40dcc8509ae621ae3f5b61020f
danielms11 <martinseguradani@gmail.com>
2023-02-24 14:20:29 +0100

    subiendo archivos

--- src/Articulos.svelte
+++ src/Articulos.svelte
@@ -1,21 +1,26 @@
 <script>
+    import { getContext } from "svelte";
+    import { data } from "./store.js";
     import { onMount } from "svelte";
     import Buscar from "./Buscar.svelte";
+    import Articulo from "./Articulo.svelte";
+    import Boton from "./Boton.svelte";
 
-    let data = [];
+    let articuloInsertar = {};
+
     let datosFiltrados = [];
     let patron = "";
 
+    const URL = getContext("URL");
+
     let getArticulos = async () => {
-        const response = await fetch(
-            "https://tiendabackend.fly.dev/api/articulos/"
-        );
-        data = await response.json();
+        const response = await fetch(URL.articulos);
+        $data = await response.json();
     };
 
     onMount(getArticulos);
 
-    $: datosFiltrados = data.filter((articulo) =>
+    $: datosFiltrados = $data.filter((articulo) =>
         RegExp(patron, "i").test(articulo.nombre)
     );
 </script>
@@ -24,6 +29,17 @@
 
 <hr />
 
+<Articulo bind:articulo={articuloInsertar}>
+    <Boton documento={articuloInsertar} />
+</Articulo>
+<br />
+
 {#each datosFiltrados as articulo}
-    {articulo.nombre} - {articulo.precio}€<br /><br />
+    <br />
+    <Articulo bind:articulo>
+        <Boton tipo="modificar" documento={articulo} />
+        <Boton tipo="eliminar" documento={articulo} />
+    </Articulo>
+    <br />
+    <br />
 {/each}
